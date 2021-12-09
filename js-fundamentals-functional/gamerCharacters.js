77392ed27b3b3664013aeed40a23d32bd311301c
Ahmad Raza <ahmadrazach26@gmail.com>
2021-12-09 21:04:45 +0500

    Completed 1st solution

--- js-fundamentals-functional/gamerCharacters.js
+++ js-fundamentals-functional/gamerCharacters.js
@@ -1,3 +1,11 @@
 var game={};
 game.murderer="??";
-game['weapons']=['lasers','angry cats','dish soap'];
+game['weapons']=[
+    {type:'lasers',location:'lab'},
+    {type:'angry cats' ...},
+    {... 'dish soap' ...}
+];
+
+game.name=[];
+game.name[0]='Miss Scarlet';
+game.name.push('Mr. Green');
