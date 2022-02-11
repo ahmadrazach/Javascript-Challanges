b5ba1d80ef39476a47b5f73104da69e74548117a
Ahmad Raza <ahmadrazach26@gmail.com>
2022-02-12 02:50:33 +0500

    Done with using foreach function to fetch the data from the API

--- movie-app/script.js
+++ movie-app/script.js
@@ -10,14 +10,37 @@ async function getMovies(){
     const respData =await resp.json(); // so that we can convert it into json 
 
     //taking the imgpath and mizing it with poster path from the API Url to take poster image
-    // respData.results.forEach(movie => {
-    //     const img=document.createElement('img'); //creating img element
-    //     img.src=IMGPATH+movie.poster_path; // giving src the img path and the movie poster path for each
+    respData.results.forEach(movie => {
+        // const img=document.createElement('img'); //creating img element
+        // img.src=IMGPATH+movie.poster_path; // giving src the img path and the movie poster path for each
 
-    //     document.body.appendChild(img);
-    // });
+        const{ poster_path,title,vote_average}=movie;
+        const movieEl=document.createElement('div');
+        movieEl.classList.add('movie');
+        movieEl.innerHTML=`
+        <img 
+            src="${IMGPATH+movie.poster_path}" 
+            alt="${title}"
+        />
+        <div class="movie-info">
+            <h3>${title}</h3>
+            <span>${vote_average}</span>
+        </div>
+        `
+        document.body.appendChild(movieEl);
+    });
     return respData;
 
 }
 
 console.log(getMovies());
+
+
+/*
+            <img src="https://image.tmdb.org/t/p/w1280/1g0dhYtq4irTY1GPXvft6k4YLjm.jpg" alt="">
+            <div class="movie-info">
+                <h3>Movie title</h3>
+                <span>8.4</span>
+            </div>
+        </div>
+*/
