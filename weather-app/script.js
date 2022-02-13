24f5727e8a458369a1f5ad359a6b930ed58102bd
Ahmad Raza <ahmadrazach26@gmail.com>
2022-02-14 03:32:54 +0500

    update script.js
    Done with showing data on the UI

--- weather-app/script.js
+++ weather-app/script.js
@@ -15,5 +15,22 @@ cityForm.addEventListener('submit',(e)=>{
     const city=cityForm.city.value.trim();
     cityForm.reset();
 
-    updateCity(city).then(data=>console.log(data)).catch(err=>console.log(err));
+    updateCity(city).then(data=>updateUI(data)).catch(err=>console.log(err));
 });
+
+
+//update the details to the front end
+const card=document.querySelector(".card");
+const details=document.querySelector(".details");
+
+const updateUI=(data)=>{
+    const{cityDetails,weather}=data;
+    console.log(cityDetails)
+    details.innerHTML=`<h5 class="my-5">${cityDetails.LocalizedName}</h5>
+    <div class="my-3">${weather.WeatherText}</div>
+    <div class="display-4 my-4">
+        <span>${weather.Temperature.Metric.Value}</span>
+        <span>&deg;C</span>
+    </div>`
+}
+
