ecf73d63fe6c80136b795435fc19420e37385deb
Ahmad Raza <ahmadrazach26@gmail.com>
2022-02-14 02:24:58 +0500

    update forecast.js
    Done with getting weather data

--- weather-app/forecast.js
+++ weather-app/forecast.js
@@ -18,3 +18,14 @@ const getCity= async (city )=>{
 getCity("Lahore")
 .then(data=>console.log(data[0]))
 .catch(err=>console.log(err));
+
+//now will be using getCity to getweather
+const getWeather=async(id)=>{
+    const baseURL="http://dataservice.accuweather.com/currentconditions/v1/";
+    const query=`${id}?apikey=${key}`;
+    const response=await fetch(baseURL+query);
+    const data=await response.json();
+    console.log(data)
+}
+
+getWeather("260622");
