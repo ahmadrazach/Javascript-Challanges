80ec1c25036c4f1509984460b0f8a67509483e0b
Ahmad Raza <ahmadrazach26@gmail.com>
2022-02-24 00:35:45 +0500

    done with taking data from the hosting

--- recipe-app/sandbox.js
+++ recipe-app/sandbox.js
@@ -1,19 +1,38 @@
-        // Import the functions you need from the SDKs you need
-        import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-app.js";
-        import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.7/firebase-analytics.js";
-          
-        // Your web app's Firebase configuration
-        // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-        const firebaseConfig = {
-          apiKey: "AIzaSyAoRtFxoCYiqLApdkymGa_QOxlqxApix6U",
-          authDomain: "devpractice-8b0e5.firebaseapp.com",
-          projectId: "devpractice-8b0e5",
-          storageBucket: "devpractice-8b0e5.appspot.com",
-          messagingSenderId: "240679929775",
-          appId: "1:240679929775:web:1bbb9f659236ffcf1e115a",
-          measurementId: "G-B0ZCJNW4EH"
-        };
-      
-        // Initialize Firebase
-        const app = initializeApp(firebaseConfig);
-        const analytics = getAnalytics(app);
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-app.js";
+  import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.6.6/firebase-analytics.js";
+  import {
+  getFirestore,
+  collection,
+  getDocs,
+  Timestamp,
+  doc,
+  setDoc,
+  addDoc,
+} from "https://www.gstatic.com/firebasejs/9.6.6/firebase-firestore.js";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAoRtFxoCYiqLApdkymGa_QOxlqxApix6U",
+  authDomain: "devpractice-8b0e5.firebaseapp.com",
+  projectId: "devpractice-8b0e5",
+  storageBucket: "devpractice-8b0e5.appspot.com",
+  messagingSenderId: "240679929775",
+  appId: "1:240679929775:web:1bbb9f659236ffcf1e115a",
+  measurementId: "G-B0ZCJNW4EH"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+//Firebase intialize
+const db=getFirestore(app);
+
+async function getRecipes(db){
+  const recepiesCol=collection(db,"receipies");
+  const recipeSnapShot=await getDocs(recepiesCol);
+  const recipeList=recipeSnapShot.docs.map((recipe)=>
+  recipe.data());
+  return recipeList;
+}
+
+const recipes=await getRecipes(db);
+console.log(recipes);
