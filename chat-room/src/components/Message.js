8b5cb642fd38d1301952b516a81bd3a553d67949
Ahmad Raza <ahmadrazach26@gmail.com>
2022-03-02 16:36:42 +0500

    Adding the Date formating library and code for the Innputed message UI

--- chat-room/src/components/Message.js
+++ chat-room/src/components/Message.js
@@ -1,3 +1,18 @@
+//creation date that we convert to a more readable format using the date-fns library
+import {formatRelative} from 'date-fns';
+
+
+const formatDate=date=>{
+    let formattedDate='';
+    if(date){
+        //Convert the date in words relative to the current date
+        formattedDate=formatRelative(date,new Date());
+        //Uppercase the first letter
+        formattedDate=formattedDate.charAt(0).toUpperCase()+formattedDate.slice(1);
+
+    }
+    return formattedDate;
+}
 const Message=({
     createdAt=null,
     text='',
@@ -5,5 +20,22 @@ const Message=({
     photoURL='',
 })=>{
     if(!text) return null;
-    return <div></div>;
-}
+    return (
+        <div>
+            {photoURL?(
+                <img
+                    src={photoURL}
+                    alt="Avatar"
+                    className="rounded-full mr-4"
+                    width={45}
+                    height={45}
+                />
+            ):null}
+            {displayName?<p>{displayName}</p>:null}
+            {createdAt?.seconds?(
+                <span>{formatDate(new Date(createdAt.seconds*1000))}</span>
+            ):null}
+            <p>{text}</p>
+        </div>
+    );
+};
