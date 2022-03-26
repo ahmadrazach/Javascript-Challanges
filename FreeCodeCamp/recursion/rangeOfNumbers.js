3bcdfcd9a3be5bdb050f4fd741d693b052c25fab
Ahmad Raza <ahmadrazach26@gmail.com>
2022-03-27 01:02:49 +0500

    Updaye

---
+++ FreeCodeCamp/recursion/rangeOfNumbers.js
@@ -0,0 +1,9 @@
+function rangeOfNumbers(startNum, endNum) {
+    if ( endNum<startNum){
+    return [];}
+    else{
+          const range=rangeOfNumbers(startNum,endNum-1)
+          range.push(endNum)
+          return range
+    }
+};
