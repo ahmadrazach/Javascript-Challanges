3f0f75f67bb2560b80eaafcab1884e07dbe120dc
Ahmad Raza <ahmadrazach26@gmail.com>
2022-04-14 02:09:54 +0500

    Done with making the Menu bar and the footer

---
+++ food-cart/frontend/src/components/Header.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import {Navbar,Nav,Container} from 'react-bootstrap'
+const Header = () => {
+  return (
+    <header>
+        <Navbar bg='dark' variant='dark'expand='lg' collapseOnSelect>
+            <Container>
+            <Navbar.Brand href='#home'>Food Cart</Navbar.Brand>
+            <Nav className='mr-auto'>
+                <Nav.Link href='/cart'>Cart  <i className='fas fa-shopping-cart'></i></Nav.Link>
+            </Nav>
+            </Container>
+        </Navbar>
+    </header>
+  )
+}
+
+export default Header
