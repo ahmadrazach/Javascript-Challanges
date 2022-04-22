77974e6f1eb33f934ca01f33a06aec38154d52fa
Ahmad Raza <ahmadrazach26@gmail.com>
2022-04-23 02:58:04 +0500

    started working on it

---
+++ FreeCodeCamp/Intermediate Algorithm Scripting/fearNotLetter.js
@@ -0,0 +1,11 @@
+function fearNotLetter(str) {
+    let z='z'
+    //take string in a loop
+    // start a counter from 97 as a's ascii is 97
+    // compare each value's ASCII with conuter value adding with every iteration
+    // if a char doesn't match ? return the char on which the counter is 
+    console.log(z.charCodeAt())
+    return str;
+  }
+  
+  fearNotLetter("abce");
