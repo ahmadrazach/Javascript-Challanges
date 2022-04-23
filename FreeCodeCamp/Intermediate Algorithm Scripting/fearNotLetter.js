acc1d59d03ea140e0f9e54ebd075e8cbe0ca0ca0
Ahmad Raza <ahmadrazach26@gmail.com>
2022-04-23 05:44:17 +0500

    done with the question

--- FreeCodeCamp/Intermediate Algorithm Scripting/fearNotLetter.js
+++ FreeCodeCamp/Intermediate Algorithm Scripting/fearNotLetter.js
@@ -1,11 +1,20 @@
 function fearNotLetter(str) {
-    let z='z'
+    let starter_char_value=str.charCodeAt(0)
+    let count_comparison=starter_char_value;
+    //console.log(starter_char_value)
     //take string in a loop
-    // start a counter from 97 as a's ascii is 97
-    // compare each value's ASCII with conuter value adding with every iteration
-    // if a char doesn't match ? return the char on which the counter is 
-    console.log(z.charCodeAt())
-    return str;
+    for(var word_index in str)
+    {
+        // start a counter from 97 as a's ascii is 97
+        // compare each value's ASCII with conuter value adding with every iteration
+        // if a char doesn't match ? return the char on which the counter is 
+
+        if(count_comparison!=str.charCodeAt(word_index))
+            return (String.fromCharCode(count_comparison))
+        else
+        count_comparison++;
+
+    }
   }
   
-  fearNotLetter("abce");
+  console.log(fearNotLetter("abcdefghjklmno"));
