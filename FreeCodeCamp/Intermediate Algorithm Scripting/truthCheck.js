b3d9474cbda22409809381be302c7969b015c46c
Ahmad Raza <ahmadrazach26@gmail.com>
2022-04-24 07:13:04 +0500

    made logic of truthCheck.js

---
+++ FreeCodeCamp/Intermediate Algorithm Scripting/truthCheck.js
@@ -0,0 +1,11 @@
+function truthCheck(collection, pre) {
+    console.log(col)
+    //loop to all collection
+    //if collection[pre]===false
+    return false
+
+    return true;
+  }
+  
+  truthCheck([{name: "Quincy", role: "Founder", isBot: false}, {name: "Naomi", role: "", isBot: false},
+   {name: "Camperbot", role: "Bot", isBot: true}], "isBot");
