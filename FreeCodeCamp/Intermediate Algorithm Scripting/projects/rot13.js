4dd09301c22cfd570497101fffdac0045b7f9284
Ahmad Raza <ahmadrazach26@gmail.com>
2022-04-26 18:05:06 +0500

    Done with the question

--- FreeCodeCamp/Intermediate Algorithm Scripting/projects/rot13.js
+++ FreeCodeCamp/Intermediate Algorithm Scripting/projects/rot13.js
@@ -1,10 +1,28 @@
 function rot13(str) {
+
     //save all the uppercaseaplhabets in a string
     //make an accumulator
+    let accumulator=""
+    const alphabet=["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"]
+    
     //loop to the str
-    // if found space or puntuation ? add it to accumulator
-    // if it's a upper case letter ? check if +13 gets you to the number or subtract the number to take the result 
-    return str;
+    for(var value_index in str)
+    {
+        //checking if it gets alphabet or space
+        const isALetter=alphabet.includes(str[value_index])
+        if(isALetter===false){
+            accumulator+=str[value_index]
+        }
+        
+               // if it's a upper case letter ? check if +13's char code >90 gets you to the number or subtract the number to take the result 
+        else{
+            const charIndex=alphabet.findIndex((c)=>c===str[value_index]);
+            
+            accumulator+= alphabet[charIndex+13] || alphabet[charIndex-13]
+        }
+
+    }
+    return accumulator;
   }
   
-  rot13("SERR PBQR PNZC");
+  console.log(rot13("SERR PBQR PNZC"));
