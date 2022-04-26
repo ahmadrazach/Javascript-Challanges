9eb45a9ddcb51192c147d0fa3e4e47497cad6bea
Ahmad Raza <ahmadrazach26@gmail.com>
2022-04-26 17:47:50 +0500

    done with the logic

---
+++ FreeCodeCamp/Intermediate Algorithm Scripting/projects/rot13.js
@@ -0,0 +1,10 @@
+function rot13(str) {
+    //save all the uppercaseaplhabets in a string
+    //make an accumulator
+    //loop to the str
+    // if found space or puntuation ? add it to accumulator
+    // if it's a upper case letter ? check if +13 gets you to the number or subtract the number to take the result 
+    return str;
+  }
+  
+  rot13("SERR PBQR PNZC");
