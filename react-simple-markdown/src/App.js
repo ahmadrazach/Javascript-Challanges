ca9f18fb553a673de8ff57bd6b4937113b2aa207
Ahmad Raza <ahmadrazach26@gmail.com>
2022-06-21 01:34:10 +0500

    Update Apps.js
    taking input and previewing it onto the output

--- react-simple-markdown/src/App.js
+++ react-simple-markdown/src/App.js
@@ -1,7 +1,16 @@
+import {useState} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  //saving value in the variable on change
+  const [input,setInput]=useState("");
+
+  const handleChange=(e)=>{
+    setInput(e.target.value);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
@@ -11,12 +20,12 @@ function App() {
       <div className='app-body'>
         <div className='input-text'>
           <h2>Input here</h2>
-          <textarea className="input-textarea"/>
+          <textarea className="input-textarea" value={input} onChange={handleChange}/>
         </div>
         <div className="preview-text">          
           <h2>Preview</h2>
           <div className="output-textarea">
-            <p>usahfduahsfuh</p>
+            <p>{input}</p>
           </div>
         </div>
       </div>
