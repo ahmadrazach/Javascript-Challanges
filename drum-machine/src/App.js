6d25d22bfbf965abff84ba1749ed7296f53503d4
Ahmad Raza <ahmadrazach26@gmail.com>
2022-06-23 21:29:42 +0500

    update

--- drum-machine/src/App.js
+++ drum-machine/src/App.js
@@ -5,7 +5,83 @@ import Button from 'react-bootstrap/Button';
 import Form from "react-bootstrap/Form";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
+
+
+//function for the Drum Buttons
+function DrumRow()
+{
+       <p>Hy</p>
+}
+
 function App() {
+
+  //sounds
+const sounds = [
+  {
+    id: 1,
+    btn:"Q",
+    src:
+      "https://s3.amazonaws.com/freecodecamp/drums/Heater-1.mp3"
+  },
+
+  {
+    id: 2,
+    btn:"W",
+    src:
+      "https://s3.amazonaws.com/freecodecamp/drums/Heater-2.mp3"
+  },
+
+  {
+    id: 3,
+    btn:"E",
+    src:
+      "https://s3.amazonaws.com/freecodecamp/drums/Heater-3.mp3"
+  },
+
+  {
+    id: 4,
+    btn:"A",
+    src:
+      "https://s3.amazonaws.com/freecodecamp/drums/Heater-4_1.mp3"
+  },
+
+  {
+    id: 5,
+    btn:"S",
+    src:
+      "https://s3.amazonaws.com/freecodecamp/drums/Heater-6.mp3"
+  },
+
+  {
+    id: 6,
+    btn:"D",
+    src:
+      "https://s3.amazonaws.com/freecodecamp/drums/Dsc_Oh.mp3"
+  },
+
+  {
+    id: 7,
+    btn:"Z",
+    src:
+      "https://s3.amazonaws.com/freecodecamp/drums/Kick_n_Hat.mp3"
+  },
+
+  {
+    id: 8,
+    btn:"X",
+    src:
+      "https://s3.amazonaws.com/freecodecamp/drums/RP4_KICK_1.mp3"
+  },
+
+  {
+    id: 9,
+    btn:"C",
+    src:
+      "https://s3.amazonaws.com/freecodecamp/drums/Cev_H2.mp3"
+  }
+];
+  
+  
   return (
     <Container fluid>
 
@@ -23,7 +99,10 @@ function App() {
             <Row className='card-row' style={{}}>
               {/* column for the buttons  */}
               <Col >
-                <Row className='m-2'>
+                {/* sounds.map((btn)=>(
+                  <DrumRow src={btn.id}/>
+                )); */}
+                {/* <Row className='m-2'>
                   <Col className="d-flex justify-content-around">
                     <Button as="input" type="button" value="Q" style={{width:"70px"}}/>
                     <Button as="input" type="button" value="W" style={{width:"70px"}}/>
@@ -43,7 +122,7 @@ function App() {
                     <Button as="input" type="button" value="X" style={{width:"70px"}}/>
                     <Button as="input" type="button" value="C" style={{width:"70px"}}/>
                   </Col>
-                </Row>
+                </Row> */}
                 
               </Col>
               {/* for the actions */}
