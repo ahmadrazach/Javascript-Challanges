10d53697d489676cb8642999625f4d4554778241
Ahmad Raza <ahmadrazach26@gmail.com>
2022-06-25 04:21:32 +0500

    Update App.js
    Done with the UI changes

--- javascript-calculator/src/App.js
+++ javascript-calculator/src/App.js
@@ -9,31 +9,44 @@ function App() {
           {/* Heading */}
           <h1 style={{textDecoration:'underline'}} className="text-center">React Javascript Calculator</h1>
           {/* Body */}
-          <div className="mt-5 w-50 m-auto"  style={{border:'1px solid black',backgroundColor:"black",height:"50vh"}}>
+          <div className="mt-5 w-50 m-auto"  style={{border:'1px solid black',backgroundColor:"black",height:"50vh",borderRadius:'5px'}}>
             {/* 6 rows */}
             {/* R1- input */}
-            <input className="w-100" style={{backgroundColor:'black',border:'none',borderBottom:'1px solid grey',height:'5vh'}} type="text" readOnly/>
-            <div className="m-2" >
+            <input className="w-100" style={{backgroundColor:'black',border:'none',borderBottom:'1px solid grey',height:'7vh'}} type="text" readOnly/>
+            <div className="m-1" >
               {/* R2 - actions (/,x,-,+)*/}
-              <Row >
-                <Col sm={"auto"}>
-                   <Button variant="primary">/</Button>
-                </Col>
-                <Col sm={"auto"}>
-                   <Button variant="primary">/</Button>
-                </Col>
-                <Col sm={"auto"}>
-                   <Button variant="primary">/</Button>
-                </Col>
-                <Col sm={"auto"}>
-                   <Button variant="primary">/</Button>
-                </Col>
-                
-              </Row>
+              <div className="d-flex justify-evenly">
+                <Button variant="secondary" className="w-25 m-1">/</Button>
+                <Button variant="secondary" className="w-25 m-1">X</Button>
+                <Button variant="secondary" className="w-25 m-1">-</Button>
+                <Button variant="secondary" className="w-25 m-1">+</Button>
+              </div>
+              
               {/* R3 - actions (=,AC)*/}
+              <div className="d-flex justify-evenly">
+                <Button variant="primary" className="w-50 m-1">=</Button>
+                <Button variant="danger" className="w-50 m-1">AC</Button>
+              </div>
               {/* R4 - btns (9,0,.)*/}
-              {/* R5 - btns (4,5,6,8) */}
+              <div className="d-flex justify-evenly">
+                <Button variant="primary" className="w-25 m-1">9</Button>
+                <Button variant="primary" className="w-25 m-1">0</Button>
+                <Button variant="primary" className="w-75 m-1">.</Button>
+              </div>
+              {/* R5 - btns (5,6,7,8) */}
+              <div className="d-flex justify-evenly">
+                <Button variant="primary" className="w-25 m-1">8</Button>
+                <Button variant="primary" className="w-25 m-1">7</Button>
+                <Button variant="primary" className="w-25 m-1">6</Button>
+                <Button variant="primary" className="w-25 m-1">5</Button>
+              </div>
               {/* R6 - btns (1,2,3,4) */}
+              <div className="d-flex justify-evenly">
+                <Button variant="primary" className="w-25 m-1">4</Button>
+                <Button variant="primary" className="w-25 m-1">3</Button>
+                <Button variant="primary" className="w-25 m-1">2</Button>
+                <Button variant="primary" className="w-25 m-1">1</Button>
+              </div>
             </div>
           </div>
     </Container>
