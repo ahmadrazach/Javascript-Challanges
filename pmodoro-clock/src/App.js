a85380d1a78cf9969c8c39caec34e68d14ba7996
Ahmad Raza <ahmadrazach26@gmail.com>
2022-06-28 01:47:39 +0500

    Update App.js
    Done with website design and working on the bigger screen

--- pmodoro-clock/src/App.js
+++ pmodoro-clock/src/App.js
@@ -1,21 +1,18 @@
 
 import Container from "react-bootstrap/Container";
 
-<style>
-  h4{
 
-  }
-</style>
 function App() {
   return (
+    <div style={{backgroundColor:'#FAFAF9'}} >
     <Container >
       {/* Heading */}
-      <h1 className="mt-3 text-center"><i className="bi bi-clock"></i> Pmodoro Clock</h1>
+      <h1 className="pt-3 text-center"><i className="bi bi-clock"></i> Pomodoro Clock</h1>
       {/* div of session and break */}
       <div className="d-flex flex-row mt-4 justify-content-between" >
         
         {/* Break Div */}
-        <div className="d-flex flex-column align-items-center" style={{border:'1px solid grey',borderRadius:'25px'}}>
+        <div className="d-flex flex-column align-items-center" style={{backgroundColor:'#E7E5E4',borderRadius:'25px'}}>
 
           <h4 className="mt-2">Break</h4>
           <i className="bi bi-lightning-charge" style={{fontSize:'4rem'}}></i>
@@ -28,7 +25,7 @@ function App() {
         </div>
 
         {/* Session Div */}
-        <div className="d-flex flex-column align-items-center" style={{border:'1px solid grey',borderRadius:'25px'}}>
+        <div className="d-flex flex-column align-items-center" style={{backgroundColor:'#E7E5E4',borderRadius:'25px'}}>
 
           <h4 className="mt-2">Session</h4>
           <i class="bi bi-pc-display-horizontal" style={{fontSize:'4rem'}}></i>
@@ -42,39 +39,45 @@ function App() {
       </div>
 
       {/* Div of  Task list and Clock*/}
-      <div className="d-flex flex-row mt-3 justify-content-around" style={{border:'1px solid black'}}>
+      <div className="d-flex flex-row  my-5 justify-content-around"  style={{borderRadius:'25px',backgroundColor:'#E7E5E4'}}>
         
         {/* Break Div */}
-        <div className="d-flex flex-column align-items-center" style={{border:'1px solid red'}}>
+        <div className="d-flex flex-column m-2 mx-5" >
 
-          <h4>Task List</h4>
-          <div className="d-flex flex-column" style={{backgroundColor:'#D9D9D9'}}>
-            <div>
-              <input type='text' placeholder="add the task here" style={{border:'none',backgroundColor:'#3A3A3A',color:'white'}}/>
-              <>+ icon</>
+          <h4 className="text-center">Task List</h4>
+          <div className="d-flex flex-column" style={{backgroundColor:'#D9D9D9',borderRadius:'10px'}}>
+            <div className="p-2" style={{backgroundColor:'#737373',color:'#FAFAF9',borderRadius:'10px'}}>
+              <input className="p-2" type='text' placeholder="Type here"  style={{border:'10px',borderRadius:"10px",backgroundColor:'#F5F5F4'}}/>
+              <strong><i className="bi bi-plus-circle btn" style={{fontSize:"1rem",fontWeight:'10px',color:'white'}}></i></strong>
             </div>
-            <div className="m-1">
-              <p style={{borderBottom:'1px dashed black'}}><i class="fa-solid fa-user"></i> 1st one</p>
-              <p style={{borderBottom:'1px dashed black'}}>1st one</p>
-              <p style={{borderBottom:'1px dashed black'}}>1st one</p>
+            <div className="m-2">
+              <p style={{borderBottom:'1px dashed black'}}><i class="bi bi-check-circle"></i> 1st one</p>
+              <p style={{borderBottom:'1px dashed black'}}><i class="bi bi-check-circle"></i> 2nd one</p>
+              <p style={{borderBottom:'1px dashed black'}}><i class="bi bi-check-circle"></i> 3rd one</p>
+              <p className="text-center">* focus on max 3 tasks *</p>
             </div>
           </div>
         </div>
 
-        {/* Session Div */}
-        <div className="d-flex flex-column align-items-center" style={{border:'1px solid red'}}>
+        {/* Clock Div */}
+        <div className="d-flex flex-column align-items-center m-2" >
 
-          <h4>Clock</h4>
-          <h3>Icon</h3>
-          <div className="d-flex">
-
-            <p>-</p>
-            <p>5m</p>
-            <p>+</p>
+          <h4 className="mt-2">Clock</h4>
+          <div className="d-flex align-items-center justify-content-center" style={{backgroundColor:"#737373",width:'170px',height:'170px',borderRadius:'50%',color:"#FAFAF9"}}>
+            <h3>25:00</h3>
+          </div>
+          <div className="d-flex mx-4 my-2">
+            
+            <i className="bi bi-play-circle mx-2 btn" style={{fontSize:'1.5rem'}}></i>
+            <i className="bi bi-pause-circle mx-2 btn" style={{fontSize:'1.5rem'}}></i>
+            <i className="bi bi-arrow-clockwise mx-2 btn" style={{fontSize:'1.5rem'}}></i>
           </div>
         </div>
       </div>
+      {/* Footer */}
+      <p className="mt-4 text-center" >made by <strong> <a href="https://github.com/ahmadrazach" style={{textDecoration:'none',color:"black"}}>Ahmed</a></strong></p>
     </Container>
+    </div>
   );
 }
 
