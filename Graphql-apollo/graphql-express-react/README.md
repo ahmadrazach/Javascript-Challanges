329bde4b2cfedabc4933e441910c33ca49e149dd
Ahmad Raza <ahmadrazach26@gmail.com>
2022-07-19 12:49:52 +0500

    connected react to graphql

--- Graphql-apollo/graphql-express-react/README.md
+++ Graphql-apollo/graphql-express-react/README.md
@@ -9,5 +9,9 @@ Packages:
 - Graphql-tag
 - nodemon
 - npm-run-all
+- bootstrap
+- reactstrap
+- apollo-boost
+- react-apollo
 
 <a href="https://developer.okta.com/blog/2018/10/11/build-simple-web-app-with-express-react-graphql#add-the-ability-to-edit-posts-in-graphql">help</a>
--- Graphql-apollo/graphql-express-react/src/index.js
+++ Graphql-apollo/graphql-express-react/src/index.js
@@ -1,17 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ApolloProvider } from 'react-apollo';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import client from './apollo'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <ApolloProvider client={client}>
+     <App />
+    </ApolloProvider>,
+   document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
+
+if (module.hot) module.hot.accept();
+
+// The module.hot.accept() isn’t really necessary,
+//  but makes it so that just the components changing within the app will refresh as you update them,
+//  rather than refreshing the entire page. Every once in a while you may need to refresh just to reset the state of the app, but generally,
+//  this leads to a quicker turnaround time.
